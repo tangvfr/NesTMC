d746043fd2f9f37ba302a9a7a8e3519a836c09d5
tangvfr <tangv.minecraft@gmail.com>
2022-06-20 23:06:04 +0200

    test input player

--- src/main/java/fr/tangv/nestmc/NesTMC.java
+++ src/main/java/fr/tangv/nestmc/NesTMC.java
@@ -1,9 +1,16 @@
 package fr.tangv.nestmc;
 
 import org.bukkit.Bukkit;
+import org.bukkit.craftbukkit.v1_8_R3.entity.CraftPlayer;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import fr.tangv.nestmc.game.TestListener;
+import io.netty.channel.Channel;
+import net.minecraft.server.v1_8_R3.EntityPlayer;
+import net.minecraft.server.v1_8_R3.PlayerConnection;
 
 /**
  * @author tangv
@@ -11,15 +18,29 @@ import fr.tangv.nestmc.game.TestListener;
  */
 public class NesTMC extends JavaPlugin {
 	
+	private TestListener listener;
+	
 	@Override
 	public void onEnable() {
-		Bukkit.getPluginManager().registerEvents(new TestListener(), this);
-		
+		this.listener = new TestListener(this);
+		Bukkit.getPluginManager().registerEvents(this.listener, this);
+		for (Player player : Bukkit.getOnlinePlayers()) {
+			EntityPlayer ep = ((CraftPlayer) player).getHandle();
+			PlayerConnection co = ep.playerConnection;
+			Channel ch = co.networkManager.channel;
+			ch.pipeline().addBefore("packet_handler", ep.getName(), this.listener);
+		}
 	}
 	
 	@Override
 	public void onDisable() {
-		
+		for (Player player : Bukkit.getOnlinePlayers()) {
+			EntityPlayer ep = ((CraftPlayer) player).getHandle();
+			PlayerConnection co = ep.playerConnection;
+			
+			Channel ch = co.networkManager.channel;
+			ch.pipeline().remove(ep.getName());
+		}
 	}
 	
 }
--- src/main/java/fr/tangv/nestmc/game/TestListener.java
+++ src/main/java/fr/tangv/nestmc/game/TestListener.java
@@ -1,43 +1,154 @@
 package fr.tangv.nestmc.game;
 
+import org.bukkit.Bukkit;
 import org.bukkit.craftbukkit.v1_8_R3.entity.CraftPlayer;
+import org.bukkit.entity.Damageable;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerChatEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.event.player.PlayerToggleSneakEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.plugin.java.JavaPlugin;
 
+import fr.tangv.nestmc.util.ReflectionUtil;
+import io.netty.channel.ChannelDuplexHandler;
+import io.netty.channel.ChannelHandler.Sharable;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelPromise;
+import net.minecraft.server.v1_8_R3.BlockPosition;
+import net.minecraft.server.v1_8_R3.EntityArmorStand;
 import net.minecraft.server.v1_8_R3.EntityHorse;
+import net.minecraft.server.v1_8_R3.EntityItemFrame;
 import net.minecraft.server.v1_8_R3.EntityPlayer;
+import net.minecraft.server.v1_8_R3.EnumDirection;
+import net.minecraft.server.v1_8_R3.GenericAttributes;
+import net.minecraft.server.v1_8_R3.PacketPlayInEntityAction;
+import net.minecraft.server.v1_8_R3.PacketPlayInEntityAction.EnumPlayerAction;
+import net.minecraft.server.v1_8_R3.PacketPlayInHeldItemSlot;
+import net.minecraft.server.v1_8_R3.PacketPlayInSteerVehicle;
+import net.minecraft.server.v1_8_R3.PacketPlayInUseEntity;
+import net.minecraft.server.v1_8_R3.PacketPlayInUseEntity.EnumEntityUseAction;
+import net.minecraft.server.v1_8_R3.PacketPlayOutAttachEntity;
+import net.minecraft.server.v1_8_R3.PacketPlayOutEntityDestroy;
+import net.minecraft.server.v1_8_R3.PacketPlayOutHeldItemSlot;
+import net.minecraft.server.v1_8_R3.PacketPlayOutSpawnEntity;
 import net.minecraft.server.v1_8_R3.PacketPlayOutSpawnEntityLiving;
 import net.minecraft.server.v1_8_R3.PlayerConnection;
 
-public class TestListener implements Listener {
+@Sharable
+public class TestListener extends ChannelDuplexHandler implements Listener {
 
-	@EventHandler
-	public void onJoin(PlayerJoinEvent e){
-		
+	private final JavaPlugin plugin;
+	private PlayerConnection co;
+	private int old;
+	private int oldd;
+	
+	public TestListener(JavaPlugin plugin) {
+		this.plugin = plugin;
 	}
 	
 	@EventHandler
-	public void onSneak(PlayerToggleSneakEvent e){
+	public void onJoin(PlayerJoinEvent e){
 		CraftPlayer player = (CraftPlayer) e.getPlayer();
 		EntityPlayer ep = player.getHandle();
-		PlayerConnection co = ep.playerConnection;
+		this.co = ep.playerConnection;
+		
+		co.networkManager.channel.pipeline().addBefore("packet_handler", ep.getName(), this);
+	}
+	
+	@EventHandler
+	public void onQuit(PlayerQuitEvent e){
+		CraftPlayer player = (CraftPlayer) e.getPlayer();
+		EntityPlayer ep = player.getHandle();
+		//co.networkManager.channel.pipeline().remove(ep.getName());
+		co.networkManager.channel.eventLoop().submit(() -> {
+			co.networkManager.channel.pipeline().remove(ep.getName());
+		});
+	}
+	
+	@EventHandler
+	public void onChat(PlayerChatEvent e){
+		if (e.getMessage().equalsIgnoreCase("test")) {
+			CraftPlayer player = (CraftPlayer) e.getPlayer();
+			EntityPlayer ep = player.getHandle();
+			this.co = ep.playerConnection;
+			
+			
+			
+			EntityHorse h = new EntityHorse(ep.getWorld());
+			this.old = h.getId();
+			//h.setInvisible(true);//work
+			h.setHealth(2F);
+			h.setType(3);
+			h.onGround = false;
+			h.setPositionRotation(ep.locX, ep.locY - 1.2, ep.locZ, 45, 0);
+			co.sendPacket(new PacketPlayOutSpawnEntityLiving(h));//spawn
+			co.sendPacket(new PacketPlayOutAttachEntity(0, ep, h));//mount
+			player.getInventory().setHeldItemSlot(4);
+			
+			EntityArmorStand ar = new EntityArmorStand(null);
+			this.oldd = ar.getId();
+			ar.setPosition(ep.locX, ep.locY - 0.2, ep.locZ);
+			//ar.setInvisible(true);//work
+			
+			co.sendPacket(new PacketPlayOutSpawnEntityLiving(ar));//spawn
+			
+		}
+	}
+	
+	@Override
+	public void channelRead(ChannelHandlerContext chx, Object ob) throws Exception {
+		if (ob instanceof PacketPlayInSteerVehicle) {
+			PacketPlayInSteerVehicle sv = (PacketPlayInSteerVehicle) ob;
+			/*sv.a()//Positive to the left 
+			sv.b()//Positive forward
+			sv.c()//jump
+			sv.d()//unmount*/
+			//Bukkit.getLogger().info("left: "+sv.a()+" forward: "+sv.b()+" jump:"+sv.c()+" unmount: "+sv.d());
+			
+			return;
+		}
+		if (ob instanceof PacketPlayInEntityAction) {
+			PacketPlayInEntityAction ea = (PacketPlayInEntityAction) ob;
+			if (ea.b() == EnumPlayerAction.OPEN_INVENTORY) {
+				Bukkit.getLogger().info("action: "+ea.b().name());
+				return;
+			}
+		}
+		if (ob instanceof PacketPlayInHeldItemSlot) {
+			PacketPlayInHeldItemSlot hi = (PacketPlayInHeldItemSlot) ob;
+			
+			Bukkit.getLogger().info("held: "+ (4-hi.a()));
+			
+			if (hi.a() == 0) {
+				co.sendPacket(new PacketPlayOutAttachEntity(0, co.player, null));//dismount
+				co.sendPacket(new PacketPlayOutEntityDestroy(this.old));//despawn horse
+				co.sendPacket(new PacketPlayOutEntityDestroy(this.oldd));//despawn itemframe
+			}
+			
+			co.sendPacket(new PacketPlayOutHeldItemSlot(4));
+			return;
+		}
+		if (ob instanceof PacketPlayInUseEntity) {
+			PacketPlayInUseEntity ue = (PacketPlayInUseEntity) ob;
+			if (ue.a() != EnumEntityUseAction.INTERACT) {
+				int id = (int) ReflectionUtil.getValue("a", ob);
+				if (id == this.oldd) {
+					Bukkit.getLogger().info("useentity: "+ ue.a().name());
+					return;
+				}
+			}
+		}
+		
+		super.channelRead(chx, ob);
+	}
+	
+	@Override
+	public void write(ChannelHandlerContext chx, Object ob, ChannelPromise chp) throws Exception {
 		
 		
 		
-		/*PacketPlayOutAttachEntity packet = new PacketPlayOutAttachEntity();
-		ReflectionUtil.setValue("a", packet, target.getEntityId());//player
-		ReflectionUtil.setValue("b", packet, mount ? this.idVehicle : -1);//vehicle
-		ReflectionUtil.setValue("c", packet, 0);//attacher par laisse
-		co.sendPacket(packet);*/
-		
-		EntityHorse h = new EntityHorse(null);
-		h.setPositionRotation(ep.locX, ep.locY, ep.locZ, ep.pitch, ep.yaw);
-		co.sendPacket(new PacketPlayOutSpawnEntityLiving(h));
-		
-		
-		
+		super.write(chx, ob, chp);
 	}
 	
 }
