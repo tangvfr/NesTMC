1cbd6c15e45c77badea30bf545b52676f99149f4
tangvfr <tangv.minecraft@gmail.com>
2022-06-26 03:01:20 +0200

    add pal file to open .data image export via gimp

---
+++ src/test/java/fr/tangv/nestmc/test/color/PalettePAL.java
@@ -0,0 +1,30 @@
+package fr.tangv.nestmc.test.color;
+
+import java.awt.Color;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import org.bukkit.map.MapPalette;
+
+public class PalettePAL {
+
+	@SuppressWarnings("deprecation")
+	public static void main(String[] args) throws IOException {
+		FileOutputStream out = new FileOutputStream("./PalettePALminecraft1.8.pal");
+		Color color;
+		for (int i = 0; i <= 143; i++) {
+			if (i >= 4) {
+				color = MapPalette.getColor((byte) i);
+				out.write(color.getRed());
+				out.write(color.getGreen());
+				out.write(color.getBlue());
+			} else {
+				out.write(0);
+				out.write(0);
+				out.write(0);
+			}
+		}
+		out.close();
+	}
+	
+}
